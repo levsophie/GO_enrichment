06cd72d5c7a50bcf41a9ae91ed8c43689d6b8503
levsophie <73026735+levsophie@users.noreply.github.com>
2021-08-13 08:15:51 +1000

    first version

---
+++ test/base.py
@@ -0,0 +1,12 @@
+import unittest
+       
+class BaseTestCase(unittest.TestCase):
+    
+    
+    def test_input(self):
+        self.assertEqual(1,1)
+
+    
+    
+if __name__ == "__main__":
+    unittest.main()
