99cd37bc181483285116fbd0078ea94a777b145a
levsophie <73026735+levsophie@users.noreply.github.com>
2021-08-14 12:43:12 +1000

    Working on layout

--- frontend/src/topbar.jsx
+++ frontend/src/topbar.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 import Box from "@material-ui/core/Box";
@@ -36,50 +36,92 @@ const useStyles = makeStyles((theme) => ({
     width: "100ch"
   },
 }));
-export default function BackToTop(props) {
+
+const CssTextField = withStyles({
+    root: {
+      '& label.Mui-focused': {
+        color: 'grey',
+      },
+      '& .MuiInput-underline:after': {
+        borderBottomColor: 'black',
+      },
+      '& .MuiOutlinedInput-root': {
+        '& fieldset': {
+          borderColor: 'grey',
+        },
+        '&:hover fieldset': {
+          borderColor: 'grey',
+        },
+        '&.Mui-focused fieldset': {
+          borderColor: 'black',
+        },
+      },
+    },
+  })(TextField);
+
+export default function TopBar(props) {
   const classes = useStyles();
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 90, hide: 'true' },
+    {
+      field: 'pvalue',
+      headerName: 'P-value',
+      width: 150,
+      editable: true,
+    },
+    {
+        field: 'test',
+        headerName: 'Number in test group',
+        width: 150,
+        editable: true,
+      },
+      {
+        field: 'control',
+        headerName: 'Number in control group',
+        width: 150,
+        editable: true,
+      },
+      {
+        field: 'description',
+        headerName: 'Description',
+        width: 150,
+        editable: true,
+      },
+]
+
+
 
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container maxWidth="lg">
-        <AppBar color='transparent'>
+      <AppBar color='transparent' variant="contained">
           <Toolbar>
-            <Typography variant="h6">
+            <Typography variant="h5">
               Functional enrichment analysis for Cryptotccus neoformans var.
               grubii
             </Typography>
           </Toolbar>
-        </AppBar>
+          </AppBar>
+      <Container maxWidth="sm">
         <Toolbar id="back-to-top-anchor" />
-        <br></br>
-        <FormControl className={classes.margin}></FormControl>
-        <TextField
+        <br></br><br></br>
+        {/* <FormControl className={classes.margin}> */}
+        <CssTextField
           id="standard-multiline-static"
           label="Multiline"
           multiline
           rows={4}
           variant="outlined"
           style={{ width: "100%" }}
-          defaultValue="Default Value"
-        ></TextField>
+        //   defaultValue="Default Value"
+          inputProps={{ style: { fontFamily: 'nunito', color: 'black'}}}
+        ></CssTextField>
+        {/* </FormControl> */}
        </Container> 
        <br></br>
-       <Container maxWidth="lg">
-        <div id="comments" className={classes.customFormControl}>
-          <FormControl className={classes.textField}>
-            <TextField
-              id="outlined-textarea"
-              label="Multiline Placeholder"
-              placeholder="Placeholder"
-              multiline
-              rows={4}
-              style={{ width: "100%" }}
-              variant="outlined"
-              inputProps={{ className: classes.textarea }}
-            />
-          </FormControl>
-        </div>
+       <Container maxWidth="sm">
+    
       </Container>
     </React.Fragment>
   );
