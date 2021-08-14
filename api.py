88678b89e4503b340c41b6b958187c4237819358
levsophie <73026735+levsophie@users.noreply.github.com>
2021-08-14 19:57:44 +1000

    Server and client are talking

--- api.py
+++ api.py
@@ -1,43 +1,3 @@
-# import pandas as pd
-# from flask import Flask, Response
-# from flask import request as req
-# from flask_restplus import Resource, Api
-# import logging
-# import urllib.request
-# import urllib, json
-# import requests
-# import sqlite3
-# from pandas.io import sql
-# import ast
-# import os.path
-# from os import path
-# from datetime import datetime
-# from flask_restplus import reqparse
-# from pprint import pprint
-# from flask import abort, make_response, jsonify
-# from flask import request
-
-# app = Flask(__name__)
-# api = Api(app)
-
-
-# session = requests.Session()
-
-# @api.response(200, "OK")
-# @api.response(400, "Bad request")
-# @app.route('/go', methods=['POST'])
-# class AnalyseGeneList(Resource):
-
-#     @api.doc("provide a list of genes")
-#     def post(self):
-#         """Analyse list of CNAG IDs"""
-#         req = request.get_json(force=True)
-#         genes = req.get('genes', None)
-#         print(genes)
-    
-    
-# if __name__ == '__main__':
-#    app.run(host='127.0.0.1', port=5194)
 
 import pandas as pd
 from flask import Flask, Response, request
@@ -57,8 +17,11 @@ from flask_restplus import reqparse
 from pprint import pprint
 from flask import abort, make_response, jsonify
 from flask_restx import Namespace, fields
+import flask_cors
 
 app = Flask(__name__)
+cors = flask_cors.CORS()
+cors.init_app(app)
 api = Api(app)
 
 
@@ -66,9 +29,9 @@ session = requests.Session()
 
 class GenesDto:
     genes = api.model(
-        "text",
+        "genes",
         {
-            "text_title": fields.List(fields.String),
+            "gene_list": fields.String,
         },
     )
 _genes = GenesDto.genes
@@ -81,16 +44,24 @@ _genes = GenesDto.genes
 class Collections(Resource):
     @api.response(200, "OK")
     @api.response(400, "Bad request")
-    @api.expect(_genes, validate=True)
+    # @api.expect(_genes, validate=True)
     # @api.marshal_with(_text, envelope="data")
     @api.doc()
     def post(self):
         req = request.get_json(force=True)
         print(req)
- 
+        return [
+      {
+        "id": 1,
+        "pvalue": 0.0004,
+        "test": 10,
+        "control": 10,
+        "description": "very interesting GO group",
+      },
+    ]
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
 
 
 
--- frontend/src/topbar.jsx
+++ frontend/src/topbar.jsx
@@ -20,8 +20,10 @@ import AccountCircle from "@material-ui/icons/AccountCircle";
 import { TextareaAutosize, Select, MenuItem } from "@material-ui/core/";
 import DateFnsUtils from "@date-io/date-fns";
 import { useHistory } from "react-router-dom";
-import { DataGrid } from '@material-ui/data-grid';
+import { DataGrid } from "@material-ui/data-grid";
 import { Button } from "@material-ui/core";
+import { useState, useEffect, useRef, Fetch } from "react";
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -35,78 +37,96 @@ const useStyles = makeStyles((theme) => ({
   },
   textarea: {
     resize: "both",
-    width: "100ch"
+    width: "100ch",
   },
 }));
 
 const CssTextField = withStyles({
-    root: {
-      '& label.Mui-focused': {
-        color: 'grey',
+  root: {
+    "& label.Mui-focused": {
+      color: "grey",
+    },
+    "& .MuiInput-underline:after": {
+      borderBottomColor: "black",
+    },
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "grey",
       },
-      '& .MuiInput-underline:after': {
-        borderBottomColor: 'black',
+      "&:hover fieldset": {
+        borderColor: "grey",
       },
-      '& .MuiOutlinedInput-root': {
-        '& fieldset': {
-          borderColor: 'grey',
-        },
-        '&:hover fieldset': {
-          borderColor: 'grey',
-        },
-        '&.Mui-focused fieldset': {
-          borderColor: 'black',
-        },
+      "&.Mui-focused fieldset": {
+        borderColor: "black",
       },
     },
-  })(TextField);
+  },
+})(TextField);
 
 export default function TopBar(props) {
   const classes = useStyles();
+  const [rows, setRows] = useState([]);
+  const [input, setInput] = useState();
 
   const columns = [
-    { field: 'id', headerName: 'ID', width: 90, hide: 'true' },
+    { field: "id", headerName: "ID", width: 90, hide: "true" },
     {
-      field: 'pvalue',
-      headerName: 'P-value',
+      field: "pvalue",
+      headerName: "P-value",
       width: 150,
     },
     {
-        field: 'test',
-        headerName: 'In test group',
-        width: 200,
-      },
-      {
-        field: 'control',
-        headerName: 'In control group',
-        width: 200,
-      },
-      {
-        field: 'description',
-        headerName: 'Description',
-        width: 500,
-      },
-]
+      field: "test",
+      headerName: "In test group",
+      width: 200,
+    },
+    {
+      field: "control",
+      headerName: "In control group",
+      width: 200,
+    },
+    {
+      field: "description",
+      headerName: "Description",
+      width: 500,
+    },
+  ];
+  // useEffect(() => {
+  //   setRows();
+  // }, []);
 
-const rows = [
-    { id: 1, pvalue: 0.0004, test: 10, control: 10, description: "very interesting GO group" },
-]
+  const handleSubmit = async () => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(input),
+    };
+    const response = await fetch("/genes", requestOptions);
+    if (response.status === 200) {
+      const rows = await response.json();
+      console.log(rows);
+      setRows(rows);
+    } else {
+      setRows([]);
+      console.log(response);
+    }
+  };
 
   return (
     <React.Fragment>
       <CssBaseline />
-      <AppBar color='transparent' variant="contained">
-          <Toolbar>
-            <Typography variant="h5">
-              Functional enrichment analysis for Cryptotccus neoformans var.
-              grubii
-            </Typography>
-          </Toolbar>
-          </AppBar>
+      <AppBar color="transparent" variant="contained">
+        <Toolbar>
+          <Typography variant="h5">
+            Functional enrichment analysis for Cryptotccus neoformans var.
+            grubii
+          </Typography>
+        </Toolbar>
+      </AppBar>
       <Container maxWidth="sm">
         <Toolbar id="back-to-top-anchor" />
-        <br></br><br></br>
-        {/* <FormControl className={classes.margin}> */}
+        <br></br>
+        <br></br>
         <CssTextField
           id="standard-multiline-static"
           label="List of identifiers (CNAG_XXXXX)"
@@ -114,38 +134,38 @@ const rows = [
           rows={4}
           variant="outlined"
           style={{ width: "100%" }}
-        //   defaultValue="Default Value"
-          inputProps={{ style: { fontFamily: 'nunito', color: 'black'}}}
+          onChange={(e) => setInput(e.target.value)}
+          inputProps={{ style: { fontFamily: "nunito", color: "black" } }}
         ></CssTextField>
-        {/* </FormControl> */}
         <Button
-            className={classes.margin}
-            type="submit"
-            variant = "contained"
-            style={{ display: "inline block" }}
-          >
-            Submit
-          </Button>
-          <Button
-            className={classes.margin}
-            type="submit"
-            variant = "contained"
-            style={{ display: "inline block" }}
-          >
-            Clear
-          </Button>
-       </Container> 
-       <br></br>
-       <Container maxWidth="lg">
-       <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize={5}
-        disableSelectionOnClick
-        autoHeight
-      />
-    </div>
+          className={classes.margin}
+          type="submit"
+          variant="contained"
+          style={{ display: "inline block" }}
+          onClick={(e) => handleSubmit(e.target.value)}
+        >
+          Submit
+        </Button>
+        <Button
+          className={classes.margin}
+          type="submit"
+          variant="contained"
+          style={{ display: "inline block" }}
+        >
+          Clear
+        </Button>
+      </Container>
+      <br></br>
+      <Container maxWidth="lg">
+        <div style={{ height: 400, width: "100%" }}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={5}
+            disableSelectionOnClick
+            autoHeight
+          />
+        </div>
       </Container>
     </React.Fragment>
   );
